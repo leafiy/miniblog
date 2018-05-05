a1ace521db98046f04d56d3f235594fbc64602ef
leafiy <t@leafiy.com>
2018-05-05 15:48:31 +0800

    update config

--- frontend/src/config.example.js
+++ frontend/src/config.example.js
@@ -4,8 +4,10 @@ export default {
     sitePort: 80,
     apiPort: 'http://qiansmile.local/api/v1',
     devPort: 8040,
+    googleMapKey:'',
   },
   production: {
+    googleMapKey:'',
     host: 'qiansmile.com',
     sitePort: 80,
     apiPort: 'https://qiansmile.com/api/v1',
