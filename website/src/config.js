4da170e4eab3395beb9dc883d279a0ecf7eebbf9
leafiy <leafiy@users.noreply.github.com>
2018-05-09 02:35:58 +0800

    ????

--- website/src/config.js
+++ website/src/config.js
@@ -4,10 +4,10 @@ export default {
     sitePort: 80,
     apiPort: 'http://qiansmile.local/api/v1',
     devPort: 8040,
-    googleMapKey:'AIzaSyBKqc37jzShn5vhv10Ae5kZG4TBYpPd-nU',
+    googleMapKey:'s-nU',
   },
   production: {
-    googleMapKey:'AIzaSyBKqc37jzShn5vhv10Ae5kZG4TBYpPd-nU',
+    googleMapKey:'asd-nU',
     host: 'qiansmile.com',
     sitePort: 80,
     apiPort: 'https://qiansmile.com/api/v1',
